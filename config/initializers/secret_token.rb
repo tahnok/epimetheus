be1d7c6b1c29e12c0cb8144c6c6ffdc34cc136f8
Wesley Ellis <tahnok@gmail.com>
2013-04-14 20:14:51 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Epimetheus::Application.config.secret_token = 'b7b8b28192c059669afb26893fbf527796abc8dee687b3dcc872971db5523ecf539bcece7db6c15495d0eb6f1790b93dae8dd9e96df9fd25b24bb9d11d878a6c'
