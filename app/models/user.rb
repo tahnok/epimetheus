98ebe6f9aee7ad10d50601e6a5092e3d207ab3d8
Wesley Ellis <tahnok@gmail.com>
2013-04-14 21:19:02 -0400

    skeleton user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string(255)
+#  name            :string(255)
+#  admin           :boolean          default(FALSE)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
+#
+
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
 class User < ActiveRecord::Base
   attr_accessible :admin, :email, :name
+  has_secure_password
+
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
+
+  before_save { |user| user.email = email.downcase }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130415001854) do
+ActiveRecord::Schema.define(:version => 20130415010937) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "name"
-    t.boolean  "admin"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.boolean  "admin",           :default => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
